44b72adef466069d6993d714cea6cfd40347599e
onyx <bhaskar.droid@gmail.com>
2019-07-11 09:11:23 +0100

    files added

--- aeroplane1.py
+++ aeroplane1.py
@@ -1,17 +1,14 @@
 def solution(N, S):
     available=0
     s = S.split(" ")
-    for i in range(1,N+1):
-        
-        seatA = str(i)+'A'
-        
+    for i in range(1,N+1):        
+        seatA = str(i)+'A'       
         if (str(i)+'A' not in s) and (str(i)+'B' not in s) and (str(i)+'C' not in s) :
             available = available + 1
         if (str(i)+'D' not in s) and (str(i)+'E' not in s) and (str(i)+'F' not in s) and (str(i)+'G' not in s):
             available = available + 1
         if (str(i)+'H' not in s) and (str(i)+'J' not in s) and (str(i)+'K' not in s):
-            available = available + 1
-    
+            available = available + 1    
     result = available
     return result
 
